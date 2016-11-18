ac246b69c05fbb78b510c8e042b7be30dbd9655f
David Román Rey <davidromanrey28@gmail.com>
2016-11-18 16:07:03 +0100

    ejemplos varios de c

---
+++ 01_cambios_de_base/base.cpp
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int decimal;
+
+    printf("Decimal:"); // muestrame en pantalla decimal.
+    scanf(" %i", &decimal); // scanf: llamada a la funcion. %i lee del buffer un numero entero. & se lle como una llamada de.
+
+    // Hacer
+    //  Imprimir decimal % 2
+    //  Decimal = decimal / 2
+    // Mientras decimal / 2 > 0
+
+    do {
+        printf("%i", decimal % 2);
+        decimal /= 2;
+    } while(decimal>0); // do es hacer y while si se cumple la condicion haz otra condicion, si no se cumple sal
+    printf("\n"); //imprimir salto de linea.
+
+    return EXIT_SUCCESS;
+}

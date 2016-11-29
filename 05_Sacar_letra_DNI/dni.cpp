95606734844ea455a5b37a4859a76ec8db517c90
David Román Rey <davidromanrey28@gmail.com>
2016-11-29 11:19:39 +0100

    modificado

--- 05_Sacar_letra_DNI/dni.cpp
+++ 05_Sacar_letra_DNI/dni.cpp
@@ -10,7 +10,7 @@ int main(){
     scanf(" %d", &dni);
     system ("clear");
 
-    if(dni<=999999999){ // Si el dni es menor a 99999999 haz:
+    if(dni<=99999999){ // Si el dni es menor a 99999999 haz:
         letra = dni%23; // Dividimos el numero entero entre 23
         printf("Tu dni es %d-%c\n",dni, letras[letra]); // imprime por pantalla %d: el numero entero que te da en la division. %c: comprueba el resultado de la operacion en el array o vector.
     }

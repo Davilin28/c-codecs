071f4a3dab714c471f0de08184dab9f7941102a9
David Román Rey <davidromanrey28@gmail.com>
2016-11-29 10:34:40 +0100

    Ejemplos de c

---
+++ 05_Sacar_letra_DNI/dni.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char letras[]="TRWAGMYFPDXBNJZSQVHLCKE"; // Esto es un array o vector para calcular la letra.
+    int dni, letra;
+
+    printf("Introduce tu dni\n");
+    scanf("%d", &dni);
+
+    if(dni<=999999999){ // Si el dni es menor a 99999999 haz:
+        letra = dni%23; // Dividimos el numero entero entre 23
+        printf("Tu dni es %d-%c",dni, letras[letra]); // imprime por pantalla %d: el numero entero que te da en la division. %c: comprueba el resultado de la operacion en el array o vector.
+        printf("\n");
+    }
+    else
+    {
+        printf("No has introducido un dni valido");
+    }
+
+    return EXIT_SUCCESS;
+}

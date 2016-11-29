683c8c6f71805c07ca5e3a0f2d8376d4d6e6418c
David Román Rey <davidromanrey28@gmail.com>
2016-11-29 11:16:55 +0100

    modificado

--- 05_Sacar_letra_DNI/dni.cpp
+++ 05_Sacar_letra_DNI/dni.cpp
@@ -7,18 +7,16 @@ int main(){
     char letras[]="TRWAGMYFPDXBNJZSQVHLCKE"; // Esto es un array o vector para calcular la letra.
 
     printf("Introduce tu dni\n");
-    scanf("%d", &dni);
+    scanf(" %d", &dni);
     system ("clear");
 
     if(dni<=999999999){ // Si el dni es menor a 99999999 haz:
         letra = dni%23; // Dividimos el numero entero entre 23
-        printf("Tu dni es %d-%c",dni, letras[letra]); // imprime por pantalla %d: el numero entero que te da en la division. %c: comprueba el resultado de la operacion en el array o vector.
-        printf("\n");
+        printf("Tu dni es %d-%c\n",dni, letras[letra]); // imprime por pantalla %d: el numero entero que te da en la division. %c: comprueba el resultado de la operacion en el array o vector.
     }
     else
     {
-        printf("No has introducido un dni valido");
+      printf("No has introducido un dni valido");
     }
-
     return EXIT_SUCCESS;
 }

ff88ffa5a1126f5956bdf60fefd9ee73d356024f
David Román Rey <davidromanrey28@gmail.com>
2017-02-09 10:15:24 +0100

    Funciones

---
+++ 10_funciones/es_par.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+bool es_par(int n){
+
+    if(n % 2 == 0)
+        return true;
+    else
+        return false;
+}
+
+
+int main(int argc, char *argv[]){
+
+    int numero;
+
+    printf("Numero: ");
+    scanf(" %i",&numero);
+
+    printf("Tu numero %s es par\n",
+           es_par(numero)? "":"no " );
+
+    return EXIT_SUCCESS;
+}

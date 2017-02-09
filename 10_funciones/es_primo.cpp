77fd25737e1b9b03198bada02ebd04a0b8a17bbd
David Román Rey <davidromanrey28@gmail.com>
2017-02-09 11:11:21 +0100

    mejorado

--- 10_funciones/es_primo.cpp
+++ 10_funciones/es_primo.cpp
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 
 bool es_primo(int n){
-    for(int i=2; i<n; i++){
+  for(int i=2; i<n; i++)
     if(n % 2 ==0)
-      return false;
-    }
-    return true;
+        return false;
+  return true;
 }
 int main(int argc, char *argv[]){
     int n;

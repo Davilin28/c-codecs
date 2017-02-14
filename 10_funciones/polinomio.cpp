5e2a66db342bbe8cc2c1b2180be4033a83773691
David Román <davidromanrey28@gmail.com>
2017-02-14 21:55:37 +0100

    Add files via upload

--- 10_funciones/polinomio.cpp
+++ 10_funciones/polinomio.cpp
@@ -3,12 +3,30 @@
 
 #define N 20
 
-double f(double pol[N], double x){
-
-    for()
-}
-
 int main(int argc, char *argv[]){
 
-   return EXIT_SUCCESS;
+	int coeficiente[N];
+	int x;
+	int potencia = 1;
+	int max_cof;
+    int resultado = 0;
+	
+	printf("Indicar numero de coeficientes: \n");
+	scanf(" %i", &max_cof);
+	
+	printf("Introducir los coeficientes: \n");
+	
+	for(int i=0; i <= max_cof; i++){
+		scanf(" %d", &coeficiente[i]);
+	}
+	
+	printf("Dime el valor de x: \n");
+	scanf(" %i",&x);
+
+    for(int j=0; j<=max_cof; j++,potencia*=x)
+		resultado+=coeficiente[j]*potencia;
+
+	printf("Resultado es: %d \n", resultado);
+	
+	return EXIT_SUCCESS;
 }

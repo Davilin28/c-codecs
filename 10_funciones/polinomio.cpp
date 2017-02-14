7fa8dceeb655b205ae22c269237572ec93420a8a
David Román Rey <davidromanrey28@gmail.com>
2017-02-14 12:01:29 +0100

    Proceso

--- 10_funciones/polinomio.cpp
+++ 10_funciones/polinomio.cpp
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 20
+
 double f(double pol[N], double x){
 
+    for()
 }
 
 int main(int argc, char *argv[]){
 
-    char numero[]="";
-
-    printf("Introducir la serie de numero: \n");
-    scanf("%c", numero);
-
-
-
-    return EXIT_SUCCESS;
+   return EXIT_SUCCESS;
 }

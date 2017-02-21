12c0080dc9f53fac63947c8d641e9432617473c6
davilin28 <davidromanrey28@gmail.com>
2017-02-21 19:42:37 +0100

    bala

--- juego/bala.cpp
+++ juego/bala.cpp
@@ -2,23 +2,38 @@
 #include <stdlib.h>
 
 #define DIM 2
-#define ax 0
-#define ay -10
-#define tiempo 0.001
+#define delta 0.1
+#define X 0
+#define Y 1
+
+double vx (double v, double x, double tiempo){
+   for(int i=0; i<tiempo; i++)
+       v += x * delta;
+   return v;
+}
+double vy (double v, double y, double tiempo){
+    for(int i=0; i<tiempo; i++)
+        v += y * delta;
+    return v;
+}
 
 int main(int argc, char *argv[]){
 
-    double vx = 0;
-    double vy = 0;
+    double tiempo;
+    double x;
+    double y;
+    double a[DIM] = {0,-10};
 
-    printf("Introducir la velocidad de x)");
-    scanf(" %lf", &vx);
+    printf("Introducir el tiempo: \n");
+    scanf(" %lf", &tiempo);
 
-    printf("Introducir la velocidad de y");
-    scanf(" %lf", &vy);
+    printf("Introducir la velocidad de x \n");
+    scanf(" %lf", &x);
 
+    printf("Introducir la velocidad de y: \n");
+    scanf(" %lf", &y);
 
-
+    printf(" La velocidad de x es: %lf y de y es: %lf\n", vx (x, a[X], tiempo), vy (y, a[Y], tiempo));
 
     return EXIT_SUCCESS;
 }

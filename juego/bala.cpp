caf4677f67963a4f693ed95492efd4c79b7aad77
David Román Rey <davidromanrey28@gmail.com>
2017-02-21 11:31:52 +0100

    Juego/Bala

---
+++ juego/bala.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DIM 2
+#define ax 0
+#define ay -10
+#define tiempo 0.001
+
+int main(int argc, char *argv[]){
+
+    double vx = 0;
+    double vy = 0;
+
+    printf("Introducir la velocidad de x)");
+    scanf(" %lf", &vx);
+
+    printf("Introducir la velocidad de y");
+    scanf(" %lf", &vy);
+
+
+
+
+    return EXIT_SUCCESS;
+}

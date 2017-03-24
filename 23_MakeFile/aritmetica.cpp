a28b77d34e3980b9664404790d28ad922d19bd06
David Román Rey <davidromanrey28@gmail.com>
2017-03-24 12:24:20 +0100

    repaso

---
+++ 23_MakeFile/aritmetica.cpp
@@ -0,0 +1,5 @@
+#include "aritmetica.h"
+
+int suma(int op1, int op2){
+    return op1 + op2;
+}
---
+++ 23_MakeFile/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "aritmetica.h"
+
+int main(int argc, char *argv[]){
+
+    printf("2 + 3 = %i \n", suma(2,3));
+
+    return EXIT_SUCCESS;
+}

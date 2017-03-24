a12188623fcb8e5be8a1de3b424c1a354ad5e9e1
David Román Rey <davidromanrey28@gmail.com>
2017-03-24 14:15:17 +0100

    struct

--- 24_struct/pila.cpp
+++ 24_struct/pila.cpp
@@ -3,10 +3,12 @@
 #include <strings.h>
 
 //#include "notas.h"
+//hacer una pila que empuje alumnos o lo que queramos.
 
 #define N 0x100
 
 struct TStack{
+    struct TAlumno *reg[1000*N];
     struct TAlumno *data[N];
     int cima;
 };

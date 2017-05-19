e57c73d154d7c8d032e84c018b4e9053d197ce76
Davilin28 <davidromanrey28@gmail.com>
2017-05-19 09:08:36 +0200

    objects, general_params, geometry

---
+++ 32_geometry/javascript/example1.js
@@ -0,0 +1,11 @@
+var arr_square = [[0,0], [0,100], [100,100], [100,0]]
+var str_square = [
+     { x: 0,   y: 0  },
+     { x: 0,   y: 100},
+     { x: 100, y: 100},
+     { x: 100, y: 0  }
+]
+
+function example1(ctx) {
+     raster(ctx, str_square)
+}
